095c8c8fa8986cacf1d4c6291189650a498a1480
folyx13 <christin.geldhof@gmail.com>
2023-09-06 16:37:42 +0200

    first commit

---
+++ src/nameValidator.js
@@ -0,0 +1,9 @@
+
+export const validateName = (nameInput, nameError) => {
+  const nameValue = nameInput.value?.trim();
+  if (nameValue === '') {
+    nameError.textContent = 'Champ obligatoire';
+  } else {
+    nameError.textContent = '';
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js', // point d'entrée
+    output: {
+        filename: 'bundle.js', // nom du fichier de sortie
+        path: path.resolve(__dirname, 'dist'), // dossier de sortie
+        clean: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/, // tous les fichiers .js
+            exclude: /node_modules/, // sauf le dossier node_modules
+            use: {
+                loader: 'babel-loader' // on utilise babel-loader
+            }
+        }, {
+            test: /\.css$/, // tous les fichiers .css
+            use: ['style-loader', 'css-loader'] // on utilise style-loader et css-loader
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            // favicon: './src/assets/img/favicon.ico',
+            title: 'Webpack App'
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
+    // devServer: {}
+}
